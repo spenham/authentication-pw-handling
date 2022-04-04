f0de9aacbf7200cd7e69d96e2885da2b2bc1b323
Spencer Hamilton <speham@gmail.com>
2022-04-04 16:59:08 -0500

    fiddling with notes authorization

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
 app.use(express.json());
+
 const {
   models: { User, Note },
 } = require("./db");
@@ -18,18 +20,24 @@ app.post("/api/auth", async (req, res, next) => {
 
 app.get("/api/auth", async (req, res, next) => {
   try {
+    console.log("NEW TEST", await User.byToken(req.headers.authorization));
     res.send(await User.byToken(req.headers.authorization));
   } catch (ex) {
     next(ex);
   }
 });
 
-app.get("/api/users/:userId/notes", async (req, res, next) => {
+app.get("/api/users/notes", async (req, res, next) => {
   try {
+    console.log("WORKING HERE: ", req.headers);
+    const verifiedToken = await jwt.verify(
+      req.headers.authorization,
+      process.env.JWT
+    );
     const user = await User.findOne({
       include: Note,
       where: {
-        id: req.params.userId,
+        id: verifiedToken,
       },
     });
     res.send(user);
